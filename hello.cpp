393a996e48f0741fd05d8f8d43f6f69e99e8df43
Om <om77@ucsb.edu>
2025-01-15 02:31:14 -0800

    add hello.cpp to repo

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Your printing code should go here
+    cout << "Hello, world!" << endl;
+    cout << "I am ready for CS16!" << endl;
+    return 0;
+}
